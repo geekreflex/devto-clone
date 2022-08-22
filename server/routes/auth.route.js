eeec998eb4dff35134b2d9d7539b54dadcae068e
jerry <jerrynwosu007@gmail.com>
2022-08-22 23:51:32 +0100

    update config vars

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -8,10 +8,11 @@ const {
   googleAuth,
   userLogoutAuth,
 } = require('../controllers/auth.controller');
+const { TWITTER_CLIENT_ID } = require('../utils/constants');
 
 router.get('/github', (_, res) => {
   res.redirect(
-    `https://github.com/login/oauth/authorize?client_id=${process.env.TWITTER_CLIENT_ID}`
+    `https://github.com/login/oauth/authorize?client_id=${TWITTER_CLIENT_ID}`
   );
 });
 router.get('/github/callback', githubAuth);
