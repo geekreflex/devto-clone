1b458f641f25f9cf76ce1df9e2bf736b1b766260
jerry <jerrynwosu007@gmail.com>
2022-08-22 19:41:22 +0100

    update#424

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,4 @@
 export const BASE_URL =
-  process.env.NODE_ENV === 'development' ? process.env.REACT_APP_API_URL : '';
+  process.env.NODE_ENV === 'development'
+    ? process.env.REACT_APP_API_URL
+    : '/api';
