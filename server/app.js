5696788f514066bb16f1772a55bdab9a8cad76e9
jerry <jerrynwosu007@gmail.com>
2022-08-22 19:19:42 +0100

    fixed client path

--- server/app.js
+++ server/app.js
@@ -32,7 +32,7 @@ app.use('/api/tags', tagRoute);
 
 // client handler
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, '/client/build')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
