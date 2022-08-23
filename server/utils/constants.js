6dd84a7e6297c00c91f5f4bbf9cb4cca1ef7a7f1
joshnwosu <joshuanwosu078@gmail.com>
2022-08-23 14:21:32 +0100

    updated server file

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -1,21 +1,21 @@
 const REDIRECT_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000'
-    : 'https://clone-devto.herokuapp.com';
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://clone-devto.herokuapp.com";
 
 const TWITTER_CLIENT_ID =
-  process.env.NODE_ENV === 'development'
+  process.env.NODE_ENV === "development"
     ? process.env.TWITTER_CLIENT_ID_DEV
     : process.env.TWITTER_CLIENT_ID_PROD;
 
 const TWITTER_CLIENT_SECRET =
-  process.env.NODE_ENV === 'development'
+  process.env.NODE_ENV === "development"
     ? process.env.TWITTER_CLIENT_SECRET_DEV
     : process.env.TWITTER_CLIENT_SECRET_PROD;
 
 const MONGO_URI =
-  process.env.NODE_ENV === 'development'
-    ? process.env.MONGO_URI_DEV
+  process.env.NODE_ENV === "development"
+    ? process.env.MONGO_URI_PROD
     : process.env.MONGO_URI_PROD;
 
 module.exports = {
